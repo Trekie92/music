4e5a37ff352721f659d4716fa9a01c0d4255df4c
Trekie92 <36585738+Trekie92@users.noreply.github.com>
2018-12-14 10:50:38 +0100

    Update musicdb.sql

--- musicdb.sql
+++ musicdb.sql
@@ -4,7 +4,7 @@ four tables for the actual music database.
 
 #User table
 CREATE TABLE user (
-userid INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+user_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 username VARCHAR(50) NOT NULL UNIQUE,
 passwd VARCHAR(50) NOT NULL,
 dbrights CHAR(1) NOT NULL DEFAULT 'a',
@@ -13,18 +13,30 @@ userrights CHAR(1) NOT NULL DEFAULT 'a'
 
 #Title table
 CREATE TABLE title (
-titleid INT AUTO_INCREMENT PRIMARY KEY,
+title_id INT AUTO_INCREMENT PRIMARY KEY,
 titlename VARCHAR(150)
 );
 
 #Album table
-CREATE TABLE title (
-albumid INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE album (
+album_id INT AUTO_INCREMENT PRIMARY KEY,
 albumname VARCHAR(150)
 );
 
 #Interpreter table
-CREATE TABLE title (
-interpreterid INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE interpreter (
+interpreter_id INT AUTO_INCREMENT PRIMARY KEY,
 interpretername VARCHAR(150)
+);
+
+#Tracklist table
+CREATE TABLE tracklist (
+title_id INT,
+interpreter_id INT,
+album_id INT,
+yearofpublic INT CHECK(yearofpublic > 1000 AND yearofpublic <= YEAR(curdate())),
+CONSTRAINT FOREIGN KEY (title_id) references title (title_id) ON DELETE RESTRICT,
+CONSTRAINT FOREIGN KEY (interpreter_id) references interpreter (interpreter_id) ON DELETE RESTRICT,
+CONSTRAINT FOREIGN KEY (album_id) references album (album_id) ON DELETE RESTRICT,
+PRIMARY KEY (title_id, interpreter_id, album_id, yearofpublic)
 );
